88690fdd9bd8670e94a50902d7d4ac015e49ea63
Arroml <107889797+Arroml@users.noreply.github.com>
2026-03-13 20:25:04 +0100

    Store screenshots as image data on clipboard

--- .config/bin/screenshot_to_clipboard.sh
+++ .config/bin/screenshot_to_clipboard.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-exec /usr/sbin/screencapture -i -c
+tmp_file="$(mktemp -t screenshot_clipboard).tiff"
+
+cleanup() {
+  rm -f "$tmp_file"
+}
+
+trap cleanup EXIT
+
+/usr/sbin/screencapture -i -x "$tmp_file" || exit 1
+
+/usr/bin/osascript <<OSA
+set the clipboard to (read (POSIX file "$tmp_file") as TIFF picture)
+OSA
