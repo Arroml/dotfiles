bf02e11783115482780b7f4ba8fb2a906e0498fd
Arroml <107889797+Arroml@users.noreply.github.com>
2026-03-13 21:11:02 +0100

    Map exact Totem hyper-b to screenshot

---
+++ .config/bin/screenshot_to_clipboard.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+tmp_file="$(mktemp -t screenshot_clipboard).tiff"
+
+cleanup() {
+  rm -f "$tmp_file"
+}
+
+trap cleanup EXIT
+
+sleep 0.25
+
+/usr/sbin/screencapture -i -x "$tmp_file" || exit 1
+
+/usr/bin/osascript <<OSA
+set the clipboard to (read (POSIX file "$tmp_file") as TIFF picture)
+OSA
